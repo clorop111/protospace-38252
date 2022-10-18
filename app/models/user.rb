f4235b4e43b913c7949844ea325f2c0d65a55d67
clorop111 <clorop111@icloud.com>
2022-10-18 15:47:31 +0900

    未ログインユーザー機能設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          has_many :comments
+         has_many :prototypes
 
   validates :name, presence: true
   validates :profile, presence: true
